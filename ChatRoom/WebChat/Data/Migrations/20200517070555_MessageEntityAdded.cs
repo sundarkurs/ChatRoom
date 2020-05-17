65b1629ba8ef3cf25395badeb1b5592762d7edb7
Sundar Urs <sundarkurs@gmail.com>
2020-05-17 12:36:42 +0530

    Added Messages Entity and updated the migration and database

---
+++ ChatRoom/WebChat/Data/Migrations/20200517070555_MessageEntityAdded.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebChat.Data.Migrations
+{
+    public partial class MessageEntityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: false),
+                    Text = table.Column<string>(nullable: false),
+                    When = table.Column<DateTime>(nullable: false),
+                    UserID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
---
+++ ChatRoom/WebChat/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebChat.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public DateTime When { get; set; }
+
+        public string UserID { get; set; }
+    }
+}
